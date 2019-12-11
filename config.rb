74cabb902ea1fbcf97a3fd5ad5f9f0dbd201aa8b
Александр Поляков <backstabe@gmail.com>
2019-12-11 19:18:14 +0300

    slim layout

--- config.rb
+++ config.rb
@@ -15,6 +15,7 @@ page '/*.txt', layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
+set :slim, format: :html
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
